3cdda446e86cf2e19699be5139ec21190a9e1489
Wong Jiang Fung <jeff@leadbook.com>
2021-07-21 10:47:34 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+en-US,en;q=0.5# Flask app with anti-scraping mechanism
+
+```sh
+poetry run flask run
+```
---
+++ app.py
@@ -0,0 +1,46 @@
+from random import randint
+
+from faker import Faker
+from flask import Flask, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
+app = Flask(__name__)
+
+Faker.seed(0)
+fake = Faker()
+
+missing_pids = [5,13,18,25]
+companies = {"{:0>4d}".format(i): fake.company() for i in range(1, 1001) if i not in missing_pids}
+
+limiter = Limiter(app, key_func=get_remote_address)
+
+
+@app.route("/")
+def listing():
+    links = ''.join(f'<li><a href="/page/{pid}">{pid}</a></li>' for pid in companies.keys())
+    return "<html><body><ul>" + \
+        links + \
+        "</ul></body></html>"
+
+
+@app.route("/page/<pid>")
+@limiter.limit("10/minute;100/hour")
+def page_detail(pid):
+    print(request.headers)
+
+    user_agent = request.headers.get('User-Agent', '')
+
+    if "Accept-Language" not in request.headers or "Chrome" not in user_agent and "Firefox" not in user_agent:
+        return "bot is not allowed", 403
+
+    if 'CSRF-Token' not in ''.join(request.cookies.keys()) and randint(1, 6) == 1:
+        return "<html><body><p>Captcha challenge: are you a bot?</p></body></html>"
+
+    try:
+        company_name = companies[pid]
+    except KeyError:
+        return "<html><body><p>Company not found</p></body></html>", 404
+    else:
+        return f'<html><body><p id="company-id">{pid}</p><p id="company-name">{company_name}</p></body></html>'
